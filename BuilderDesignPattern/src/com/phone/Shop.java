77ecc23dfc01a59a095a642d948d14f3a39dca10
niralinaik <niralip2011@gmail.com>
2018-05-09 00:46:39 -0400

    Added print Statement.

--- BuilderDesignPattern/src/com/phone/Shop.java
+++ BuilderDesignPattern/src/com/phone/Shop.java
@@ -4,7 +4,9 @@ public class Shop {
 
 	public static void main(String[] args) {
 		
-		Phone p = new Phone("Android", "intel", 6, 1100, 3);
+		Phone p = new Phone("Nokia", "intel", 6, 1100, 3);
+		
+		System.out.println(p);
 	}
 
 }
